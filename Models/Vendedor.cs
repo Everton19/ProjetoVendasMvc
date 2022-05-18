7b295e1c9ff3a19a08457ec372451c93a1d95cc0
Everton Teixeira <evertontp5@gmail.com>
2022-05-18 20:20:42 -0300

    Localização e Formatação de números e datas

--- Models/Vendedor.cs
+++ Models/Vendedor.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace ProjetoAspVendas.Models
@@ -8,8 +9,16 @@ namespace ProjetoAspVendas.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Display(Name = "Salário Base")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double SalarioBase { get; set; }
+
+        [Display(Name = "Data de Nascimento")]
+        [DataType(DataType.Date)]
         public DateTime DataNascimento { get; set; }
         public Departamento Departamento { get; set; }
         public int DepartamentoId { get; set; }
